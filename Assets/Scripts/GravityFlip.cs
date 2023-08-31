73c56842b431039fcee73f10922b0936f072c3f7
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-08-31 18:12:38 -0400

    Gravity Trails
    Not Done
    
    Sea Farty
    
    Ehe
    
    Frost Flop

---
+++ Assets/Scripts/GravityFlip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFlip : MonoBehaviour
+{
+    public Rigidbody2D avatarRigidbody;
+    public GameObject avatar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            avatarRigidbody.gravityScale *= -1;
+            Vector3 newDirection = avatar.transform.localScale;
+            newDirection.y *= -1;
+            avatar.transform.localScale = newDirection;
+        }
+    }
+}
